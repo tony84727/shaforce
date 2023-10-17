2ec9d636c5bbf7a60e80fdd8ab3e185d7d616f9c
Tony Duan <tony84727@gmail.com>
2023-10-17 12:53:01 +0800

    feat: calculate hash per seconds

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,43 @@
-use std::ops::RangeInclusive;
+use std::time::Duration;
+use std::{ops::RangeInclusive, sync::mpsc::channel};
 
-use rayon::prelude::{ParallelBridge, ParallelIterator};
+use itertools::Itertools;
+use rayon::prelude::*;
 use sha1::{Digest, Sha1};
 
 const CHARS: RangeInclusive<char> = '!'..='~';
 
-struct Permutation {
-    stack: Vec<(RangeInclusive<char>, String)>,
-    max_length: usize,
-}
-
-impl Iterator for Permutation {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.stack.pop() {
-            Some((mut chars, prefix)) => match chars.next() {
-                Some(c) => {
-                    let next = format!("{prefix}{c}");
-                    if next.len() < self.max_length {
-                        self.stack.push((CHARS.into_iter(), next.clone()));
-                    }
-                    self.stack.push((chars, prefix));
-                    Some(next)
-                }
-                None => self.next(),
-            },
-            None => None,
-        }
-    }
-}
-
-impl Permutation {
-    fn new(max_length: usize) -> Self {
-        Self {
-            stack: vec![(CHARS.into_iter(), String::new())],
-            max_length,
-        }
-    }
-}
-
 fn main() {
-    let p = Permutation::new(8);
-    p.par_bridge()
-        .map(|input| {
-            let mut hash = Sha1::new();
-            hash.update(input.as_bytes());
-            let hash = hash.finalize();
-            format!("{input}\t{hash:x}")
-        })
-        .for_each(|result| {
-            println!("{result}");
-        })
+    let mut counter = 0;
+    let mut last = std::time::Instant::now();
+    let second = Duration::from_secs(1);
+    let (sender, receiver) = channel();
+    std::thread::spawn(move || {
+        (0..8)
+            .into_par_iter()
+            .flat_map(|length| {
+                CHARS
+                    .permutations(length)
+                    .par_bridge()
+                    .map(|chars| chars.into_iter().collect::<String>())
+            })
+            .map(|input: String| {
+                let mut hash = Sha1::new();
+                hash.update(input.as_bytes());
+                let hash = hash.finalize();
+                format!("{input}\t{hash:x}")
+            })
+            .for_each(|_result| {
+                sender.send(()).unwrap();
+            });
+    });
+    for _ in receiver.into_iter() {
+        counter += 1;
+        let now = std::time::Instant::now();
+        if now - last >= second {
+            println!("{counter}/s");
+            counter = 0;
+            last = now;
+        }
+    }
 }
